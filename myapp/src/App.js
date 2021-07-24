5c976294987f1d14ee09339501639592f3019305
TNQA1 <81133854+TNQA1@users.noreply.github.com>
2021-07-24 22:06:44 +0700

    deploytesting!

---
+++ myapp/src/App.js
@@ -0,0 +1,20 @@
+function Hello(){
+  return <span>Hello</span>
+ }
+ 
+ function Header(){
+  return <h1>Header hekkli Test</h1>
+}
+ 
+ 
+ function App() {
+  return (
+    <div>
+    <Hello/>
+    <Header/>
+    </div>
+  );
+ }
+  
+
+export default App;
